e202343068524d1aabe0d1e2dd4b45f0c468dc70
Sehmon <sehmon3@gmail.com>
2020-05-03 11:08:00 -0700

    Initial socket.io setup

--- pages/NowShowing.js
+++ pages/NowShowing.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
 import Layout from '../components/Layout';
 
+const socket = io('http://localhost:3000');
+
 export default function NowShowing() {
+  useEffect(() => {
+    socket.on('message', data => {
+      console.log(data);
+      // socket.emit('my other event', { my: 'data' });
+    });
+  }, []);
+
   return <Layout>Now showing page</Layout>;
 }
