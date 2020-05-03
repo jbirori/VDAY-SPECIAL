612f30524971e7eed1b703b954ee5e581afca24b
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-05-03 15:14:58 -0400

    Add an environment variable to set the socketURL

--- pages/NowShowing.js
+++ pages/NowShowing.js
@@ -2,7 +2,11 @@ import React, { useEffect } from 'react';
 import io from 'socket.io-client';
 import Layout from '../components/Layout';
 
-const socket = io('http://localhost:3000');
+const socketURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://clapcitycinema.herokuapp.com'
+    : 'http://localhost:3000';
+const socket = io(socketURL);
 
 export default function NowShowing() {
   useEffect(() => {
