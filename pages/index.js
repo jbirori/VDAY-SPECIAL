5f53e4dc419abdf805c90977d62f0131621ac262
jbirori <jeffbirori@gmail.com>
2020-05-12 23:36:43 -0700

    Structure home page. Still waiting on svg of marquee (and admit one ticket?) so that we don't need to reupload images every week

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 
-import Layout from '../components/Layout';
-
 const baseUrl =
   process.env.NODE_ENV === 'production'
     ? 'https://clapcitycinema.herokuapp.com'
@@ -12,36 +10,97 @@ const baseUrl =
 
 export default function Index({ isLive }) {
   return (
-    <Layout>
-      <div className="home-page">
-        {!isLive && <img className="disabled" src="/admit_one.png" alt="Admission Ticket" />}
+    <div className="home-page">
+      <div className="home-page-image" />
+      <div className="home-page-filter" />
+      <img className="logo" src="/CCCLogo.png" alt="Clap City Cinema Logo" />
+      <div className="showing-content">
+        <img className="marquee" src="/blue_marquee.png" alt="May 13th, 8pm" />
         {isLive && (
           <Link href="/now-showing">
-            <img className="admit" src="/admit_one.png" alt="Admission Ticket" />
+            <img className="admit" src="/blue_admit_one.png" alt="Admission Ticket" />
           </Link>
         )}
       </div>
 
-      <style jsx>
+      <style jsx global>
         {`
+          html {
+            position: relative;
+          }
+
+          html,
+          body,
+          #__next {
+            height: 100%;
+            width: 100%;
+            margin: 0px !important;
+          }
+
           .home-page {
             width: 100%;
             height: 100%;
             display: flex;
-            justify-content: center;
+            flex-direction: column;
             align-items: center;
+            flex: 1 1 auto;
+          }
+
+          .logo {
+            max-width: 50%;
+            margin-top: 60px;
+          }
+
+          .showing-content {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center;
+          }
+
+          .showing-content img {
+            margin: 30px 0;
+          }
+
+          .marquee {
+            max-width: 75%;
+            max-height: 30vh;
           }
 
           .admit {
             cursor: pointer;
+            max-width: 40%;
           }
 
-          .disabled {
-            opacity: 0.3;
+          .home-page-image {
+            position: fixed;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            z-index: -1;
+
+            background-image: url('/CCC-bkg.png');
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+          }
+
+          .home-page-filter {
+            position: fixed;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            z-index: -1;
+            opacity: 0.9;
+            background-color: ${isLive ? '#1B1B1B' : '#FFFFFF'};
           }
         `}
       </style>
-    </Layout>
+    </div>
   );
 }
 
