51e00c3c5ea3d3fd691fee7723b4e44b5db77e56
David Latimore II <david.latimore@me.com>
2020-05-13 10:14:13 -0400

    slight anim for the admit btn

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
@@ -10,15 +10,25 @@ const baseUrl =
     : 'http://localhost:3000';
 
 export default function Index({ isLive }) {
+  const [ ticketHover, setTicketHover ] = useState(false);
+  const toggleTicketHover = () => setTicketHover(!ticketHover);
   return (
     <Layout theme='dark'>
       <div className="home-page">
         <div className='overlay'></div>
-        {!isLive && <img className="disabled" src="/admit_one.png" alt="Admission Ticket" />}
+        {!isLive && (
+          <div className='body'>
+          <img className="marquee" src="/marquee-title.png" alt="Lineup" />
+            <img className="disabled" src="/admit_one.png" alt="Admission Ticket" />
+          </div>
+        )}
         {isLive && (
-          <Link href="/now-showing">
-            <img className="admit" src="/admit_one.png" alt="Admission Ticket" />
-          </Link>
+          <div className='body'>
+            <img className="marquee" src="/marquee-title.png" alt="Lineup" />
+            <Link href="/now-showing">
+              <img style={hoverStyles} className="admit" src="/admit_one.png" onMouseOver={toggleTicketHover} onMouseOut={toggleTicketHover} alt="Admission Ticket" />
+            </Link>
+          </div>
         )}
       </div>
 
@@ -44,6 +54,19 @@ export default function Index({ isLive }) {
             z-index: 1;
           }
 
+          .home-page .body {
+            display: flex;
+            flex-direction: column;
+            z-index: 1;
+            width: 90%;
+            align-items: center;
+            position: relative;
+          }
+
+          .marquee {
+            margin-bottom: 35px;
+          }
+
           .bkg {
             position: absolute;
             top: 0;
@@ -63,6 +86,8 @@ export default function Index({ isLive }) {
 
           .admit, .disabled {
             z-index: 2;
+            width: 125px;
+            transform: ${ticketHover ? 'rotate(14deg)' : null};
           }
         `}
       </style>
