3a64e71291b5a7e2df54fc88a2a9690d00bbd4dc
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-05-17 12:17:29 -0400

    Add rough message list, message, and composed chat

---
+++ components/Chat.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import io from 'socket.io-client';
+import TitleBar from './TitleBar';
+import MessageForm from './MessageForm';
+import MessageList from './MessageList';
+
+const socketURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://clapcitycinema.herokuapp.com'
+    : 'http://localhost:3000';
+const socket = io(socketURL);
+
+function Chat() {
+  return (
+    <Chat.Container>
+      <TitleBar />
+      <MessageList />
+      <MessageForm socket={socket} />
+    </Chat.Container>
+  );
+}
+
+Chat.Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 800px;
+  min-width: 500px;
+  width: 80%;
+  height: 80%;
+  min-height: 300px;
+  position: absolute;
+  border-radius: 5px;
+  top: 50%;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+  background-color: white;
+  transform: translateY(-53%);
+  box-shadow: 2px 7px 10px rgba(0, 0, 0, 0.5);
+`;
+
+export default Chat;
